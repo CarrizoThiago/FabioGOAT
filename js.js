3c0fa90df85f888926a5c134d257fed194507675
CarrizoHamThiago <hamthiago08@gmail.com>
2026-04-25 16:20:05 -0300

    1er ejercicio

--- js.js
+++ js.js
@@ -1,1 +1,15 @@
-document.writeln("papoi");
+/*1) Tratemos 5 operaciones de array.
+● Crear un array con los items “Jazz” y “Blues”.
+● Agregar “Rock-n-Roll” al final.
+● Reemplazar el valor en el medio por “Classics”. Tu código para
+encontrar el valor medio debe funcionar con cualquier array de longitud
+impar.
+● Quitar el primer valor del array y mostrarlo.
+● Anteponer Rap y Reggae al array.*/
+
+let array = ["Jazz", "Blues"];
+array.push("Rock-n-Roll");
+array[Math.floor(array.length / 2)] = "Classics";
+console.log(array.shift());
+array.unshift("Rap", "Reggae");
+console.log(array);
